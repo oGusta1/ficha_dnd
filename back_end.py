20b0da7b5636e3b161aa100cad61500a50886e5b
oGusta1 <gustavofigueiredo15112004@gmail.com>
2025-10-30 14:54:47 -0300

    Update back_end.py

--- back_end.py
+++ back_end.py
@@ -1,33 +1,39 @@
 import random
 
 class Dados:
+    def __init__(self):
+        self._rng = random
     
-    d4 : int
-    d6 : int
-    d8 : int
-    d10 : int
-    d12 : int 
-    d20 : int
-    
-    def d4():
-        return random.randint(1,4)
+    def d4(self) -> int:
+        return self._rng.randint(1,4)
 
-    def d6():
-        return random.randint(1,6)
+    def d6(self) -> int:
+        return self._rng.randint(1,6)
     
-    def d8():
-        return random.randint(1,8)
+    def d8(self) -> int:
+        return self._rng.randint(1,8)
     
-    def d10():
-        return random.randint(1,10)
+    def d10(self) -> int:
+        return self._rng.randint(1,10)
     
-    def d12():
-        return random.randint(1,12)
+    def d12(self) -> int:
+        return self._rng.randint(1,12)
     
-    def d20():
-        return random.randint(1,20)
+    def d20(self) -> int:
+        return self._rng.randint(1,20)
     
-class Ficha_dnd:
+class Atributo:
+    
+    def __init__(self, ponto_atributo: int = 0):
+        self.ponto_atributo: int = int(ponto_atributo)
+        self.modificador: int = self.calcular_modificador()
+
+    def calcular_modificador(self) -> int:
+        mod = (self.ponto_atributo - 10) // 2
+        self.modificador = mod
+        return mod
+   
+class Ficha:
     nome : str
     atributo : int
     classe : str 
@@ -35,26 +41,65 @@ class Ficha_dnd:
     vida : int
     ca : int
     iniciativa : int
-    
-    def __init__(self, nome, atributo, classe, raca, ability, vida, ca,iniciativa):
+
+    vida_regras = {
+    "Ladino": {"dado": 8, "min": 5},
+    "Guerreiro":{"dado": 10, "min": 6},
+    "Bárbaro": {"dado": 12, "min": 7},
+}
+
+    def __init__(self, nome, atributo, classe, raca, forca, constituicao, destreza, inteligencia, sabedoria, carisma):
         self.nome = nome
         self.atributo = atributo
         self.classe = classe
         self.raca = raca
-        self.ability = ability
-        self.vida = vida
-        self.ca = ca
-        self.iniciativa = iniciativa
+        self.vida = self.ca_vida()
+        self.ca = self.calculo_vida
+        self.iniciativa = 0
 
+        self.forca: Atributo = Atributo(forca)
+        self.constituicao: Atributo = Atributo(constituicao)
+        self.destreza: Atributo = Atributo(destreza)
+        self.inteligencia: Atributo = Atributo(inteligencia)
+        self.sabedoria: Atributo = Atributo(sabedoria)
+        self.carisma: Atributo = Atributo(carisma)
 
-class Atributos:
+        self.dado = Dados()
+
+    def calculo_vida(self):
+        regra = self.vida_regras.get(self.classe)
+        
+        faces = regra["dado"]
+        minimo = regra ["min"]
+
+        roll = self.dado.rolar(faces)
+        base = max(roll, minimo)
+        self.vida = max(base + self.constituicao.modificador,1)
+
+        return
     
-    forca : 0
-    destreza : 0
-    constituicao : 0
-    inteligencia : 0
-    sabedoria : 0
-    carisma : 0
+    def ca_ficha(self) -> int:
+
+        return 10 + self.destreza.modificador
+
+    def iniciativa(self) -> int:
+        rolagem = self.dado.d20()
+        mod_dex = self.destreza.modificador
+        return rolagem + mod_dex
+    
+
+    def __str__(self) -> str:
+        return {
+            "nome": self.nome,
+            "raca": self.raca,
+            "classe": self.classe,
+            "forca":{"pontos de forca":self.forca.ponto_atributo, "mod": self.forca.modificador},
+            "destreza":{"pontos de destreza": self.destreza.ponto_atributo, "mod": self.destreza.modificador},
+            "constituicao":{"pontos de constituicao": self.constituicao.ponto_atributo, "mod": self.constituicao.modificador},
+            "inteligencia":{"pontos de inteligencia": self.inteligencia.ponto_atributo, "mod": self.inteligencia.modificador},
+            "sabedoria": {"pontos de sabedoria": self.sabedoria.ponto_atributo, "mod": self.sabedoria.modificador},
+            "carisma": {"pontos de carisma": self.carisma.ponto_atributo, "mod": self.carisma.modificador},
+        }
 
 
 
